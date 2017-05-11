2f7d1833e44e9012dd598ff5cabfb9120608de7b
okadakosuke <okada.kosuke@m.alhinc.jp>
2017-05-11 18:37:21 +0900

    chapter6

---
+++ src/chapter6/controller/TopServlet.java
@@ -0,0 +1,37 @@
+package chapter6.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import chapter6.beans.User;
+
+
+@WebServlet(urlPatterns = { "/index.jsp" })
+public class TopServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws IOException,
+ServletException{
+
+
+		User user = (User) request.getSession().getAttribute("loginUser");
+		boolean isShowMessageForm;
+		if (user != null) {
+			isShowMessageForm = true;
+		} else {
+			isShowMessageForm = false;
+		}
+
+		request.setAttribute("isShowMessageForm", isShowMessageForm);
+
+		request.getRequestDispatcher("/top.jsp").forward(request, response);
+	}
+
+}
